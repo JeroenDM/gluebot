0b2c5206f986757d50c04c98fabffd9282a20359
jeroendm <jeroendemaeyer+ur@live.be>
2018-11-21 16:06:17 +0100

    create halcon service using a tcp socket connection from python

--- gluebot_app/scripts/halcon_client.py
+++ gluebot_app/scripts/halcon_client.py
@@ -3,19 +3,21 @@
 import sys
 import rospy
 
+from geometry_msgs.msg import Pose2D
+
 from gluebot_app.srv import *
 
-def get_pose(val):
+def get_pose():
     rospy.wait_for_service('/halcon')
     try:
         ser = rospy.ServiceProxy('/halcon', GetPose2D)
-        res = ser(val)
-        return res.message
+        res = ser()
+        return res.pose
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
 
 if __name__ == "__main__":
     print("Requesting True")
-    print(get_pose(True))
+    print(get_pose())
    
--- gluebot_app/scripts/halcon_server_example.py
+++ gluebot_app/scripts/halcon_server_example.py
@@ -1,48 +1,57 @@
 #!/usr/bin/env python
 
 import socket
+import json
 
-
+# settings
 TCP_IP = 'localhost'
 TCP_PORT = 9090
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
 
-message = '{ "op": "subscribe", "topic": "/chatter" }'
-
-service_msgs = '{"op": "advertise_service", "type": "std_srvs/SetBool", "service": "/halcon"}'
-
+# connect to rosbridge server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
-#test_data = '{"args": {"data": true}, "id": "service_request:/halcon:1", "service": "/halcon", "op": "call_service"}'
+# send json string to advertise service
+service_msgs = '{"op": "advertise_service", "type": "gluebot_app/GetPose2D", "service": "/halcon"}'
+s.send(service_msgs)
 
+# function to create the response string with the same id as the request
+def create_response(request_msg):
+  """ create response string
 
+  The id in the response must match the id in the request.
+  We use the json library to convert the rosbridge string into
+  a python dict object.
 
-def create_response(id):
+  """
+
+  request_data = json.loads(request_msg)
+  request_id = '"' + request_data['id'] + '"'
+
   res = '{ "op": "service_response",'
-  res += '"id": ' + id
+  res += '"id": ' + request_id + ', '
   res += '"service": "/halcon",'
-  res += '"values": {"success": true, "message": "I got the call"},'
-  res += '"result": true,'
+  res += '"values": {"pose": {"x": 0.1, "y": 0.2, "theta": 0.3}},'
+  res += '"result": true'
   res += '}'
   return res
 
-response= '{ "op": "service_response", "id": "service_request:/halcon:1", "service": "/halcon", "values": {"success": true, "message": "I got the call"}, "result": true}'
 
-#
-
-
-
-#s.send(message)
-s.send(service_msgs)
-
+# keep listening for service requests
 while(True):
-  data = s.recv(BUFFER_SIZE)
-  print "received data:", data
-  if (len(data) > 0):
-    s.send(response)
-    #s.send(create_response('"service_request:/halcon:1"'))
+  req = s.recv(BUFFER_SIZE)
+  if (len(req) > 0):
+
+    print("Client received request:")
+    print(req)
+
+    resp = create_response(req)
+
+    print("Client sending response:")
+    print(resp)
+
+    s.send(resp)
 
 
 s.close()
